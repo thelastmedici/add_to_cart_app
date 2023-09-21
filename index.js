71e9553a289146b9026871769aeb09c5751f8f17
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2023-09-21 15:22:42 +0100

    added functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-1fb49-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListItemsEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ""
+    
+    shoppingListItemsEl.innerHTML += `<li>${inputValue}</li>`
+})
